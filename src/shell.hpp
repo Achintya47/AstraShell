de03a2439f31c359a9b36aa6106bc19429146ab1
Achintya Sharma <sharmaachintya49@gmail.com>
2026-01-18 20:46:05 +0000

    Add -> Add struct JobState

--- src/shell.hpp
+++ src/shell.hpp
@@ -1,5 +1,18 @@
 #pragma once
 #include <string>
+#include <vector>
+
+enum class JobState {
+    Running,
+    Stopped
+};
+
+struct Job {
+    int job_id;
+    pid_t pgid;
+    std::string command;
+    JobState state;
+};
 
 class Shell{
     public :
@@ -8,4 +21,8 @@ class Shell{
         void print_prompt() const;
         std::string read_line() const;
         void execute_line(const std::string& line);
+        void check_background_jobs();
+
+        std::vector<Job> jobs_;
+        int next_job_id_ = 1;
 };
