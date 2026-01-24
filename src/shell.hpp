c00b1b83833b8f11a18fd8babd503d279c1787f9
Achintya Sharma <sharmaachintya49@gmail.com>
2026-01-24 11:58:47 +0000

    Update Shell's header

--- src/shell.hpp
+++ src/shell.hpp
@@ -22,7 +22,12 @@ class Shell{
         std::string read_line() const;
         void execute_line(const std::string& line);
         void check_background_jobs();
+        void execute_pipeline(const std::vector<std::string>& tokens, bool background);
+        std::string join_tokens(const std::vector<std::string>& tokens);
 
+
+        // Process group is a set of related processes treated as one job
+        pid_t shell_pgid; // For global acess
         std::vector<Job> jobs_;
         int next_job_id_ = 1;
 };
